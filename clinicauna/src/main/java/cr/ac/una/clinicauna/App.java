00c599bbd8fbaac0aab9aca1e1c75f40116f94a9
enajera123 <estebannajera42@gmail.com>
2023-10-02 15:18:00 -0600

    [ADD]Login View

--- clinicauna/src/main/java/cr/ac/una/clinicauna/App.java
+++ clinicauna/src/main/java/cr/ac/una/clinicauna/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(getFXMLLoader("Login").load());
-        scene.getStylesheets().add("https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap");
+        //scene.getStylesheets().add("https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap");
         stage.setScene(scene);
         stage.setFullScreen(true);
         stage.show();
