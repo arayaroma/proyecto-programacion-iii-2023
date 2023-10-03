46a838d4d82e34784005e8b7dfcd632d0fdc4fdf
Daniel Araya <darayaroma@gmail.com>
2023-10-03 16:32:51 -0600

    [ADD] SEQ_DOCTOR

--- clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Database.java
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Database.java
@@ -4,4 +4,5 @@ public class Database {
 
    public static final String SCHEMA = "CLINICAUNA";
    public static final String SEQ_USER = "SEQ_USER";
+   public static final String SEQ_DOCTOR = "SEQ_DOCTOR";
 }
