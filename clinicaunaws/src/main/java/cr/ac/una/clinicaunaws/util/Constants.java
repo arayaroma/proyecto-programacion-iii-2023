2da9b4ea82f8fda6fb3ded1093336861dff3d324
Daniel Araya <darayaroma@gmail.com>
2023-10-03 03:08:33 -0600

    [ADD] Utility Classes

---
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Constants.java
@@ -0,0 +1,13 @@
+package cr.ac.una.clinicaunaws.util;
+
+/**
+ * 
+ * @author arayaroma
+ */
+public class Constants {
+    public static final String COMPANY_NAME = "ClinicaUNA";
+    public static final String WEB_BASE_DIR = "/clinicaunaws/";
+    public static final String PORT = "8080";
+    public static final String MAIL_USER = "clinicaunaws@gmail.com";
+    public static final String MAIL_PASSWORD = "ezsuntnlmidvvrzn";
+}
---
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Database.java
@@ -0,0 +1,7 @@
+package cr.ac.una.clinicaunaws.util;
+
+public class Database {
+
+   public static final String SCHEMA = "CLINICAUNA";
+   public static final String SEQ_USER = "SEQ_USER";
+}
