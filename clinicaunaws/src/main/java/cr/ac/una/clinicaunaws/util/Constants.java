ecea99cd34b103dcab15b8bfbf56156bf8b883d7
Daniel Araya <darayaroma@gmail.com>
2023-10-03 16:16:29 -0600

    [FIX] gmail API Credentials

--- clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Constants.java
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Constants.java
@@ -9,5 +9,5 @@ public class Constants {
     public static final String WEB_BASE_DIR = "/clinicaunaws/";
     public static final String PORT = "8080";
     public static final String MAIL_USER = "clinicaunaws@gmail.com";
-    public static final String MAIL_PASSWORD = "ezsuntnlmidvvrzn";
+    public static final String MAIL_PASSWORD = "aaxsmampvktmkput";
 }
