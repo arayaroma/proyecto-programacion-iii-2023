2c2415ceeceacbd48a992a6b20f53ca6ac69e711
Daniel Araya <darayaroma@gmail.com>
2023-10-03 13:04:30 -0600

    [ADD API PACKAGE] for testing the API

---
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/api/createUser.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+curl -X POST -H "Content-Type: application/json" -d '{
+ "id": null,
+  "username": "user1",
+  "password": "password1",
+  "name": "Elliot",
+  "firstLastname": "Alderson",
+  "secondLastname": "Wellick",
+  "identification": "000000000",
+  "email": "elliot@alderson.com",
+  "role": "ADMINISTRATOR",
+  "phoneNumber": "00000000",
+  "isActive": "Y",
+  "isAdmin": "N",
+  "passwordChanged": "N",
+  "activationCode": null,
+  "language": "ENGLISH",
+  "profilePhoto": null,
+  "version": 0
+}' http://localhost:8080/clinicaunaws/api/UserController/create
