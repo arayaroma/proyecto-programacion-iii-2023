71ef784a6fc80ad2340a653332117fedf59bda0d
Daniel Araya <darayaroma@gmail.com>
2023-10-04 00:59:44 -0600

    [ADD] Entity And DTO Agenda

---
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/dto/AgendaDto.java
@@ -0,0 +1,50 @@
+package cr.ac.una.clinicaunaws.dto;
+
+import cr.ac.una.clinicaunaws.entities.Agenda;
+import cr.ac.una.clinicaunaws.util.DtoMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author arayaroma
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgendaDto implements DtoMapper<Agenda, AgendaDto> {
+
+    private Long id;
+    private DoctorDto doctor;
+    // private PatientCareDto patientCare;
+    private String date;
+    private String shiftStartTime;
+    private String shiftEndTime;
+    private Long hourlySlots;
+    private Long version;
+
+    @Override
+    public AgendaDto convertFromEntityToDTO(Agenda entity, AgendaDto dto) {
+        return new AgendaDto(entity);
+    }
+
+    @Override
+    public Agenda convertFromDTOToEntity(AgendaDto dto, Agenda entity) {
+        return new Agenda(dto);
+    }
+
+    /**
+     * @param agenda constructor from entity to dto
+     */
+    public AgendaDto(Agenda entity) {
+        this.id = entity.getId();
+        this.doctor = null;
+        this.date = entity.getDate().toString();
+        this.shiftStartTime = entity.getShiftStartTime();
+        this.shiftEndTime = entity.getShiftEndTime();
+        this.hourlySlots = entity.getHourlySlots();
+        this.version = entity.getVersion();
+    }
+
+}
