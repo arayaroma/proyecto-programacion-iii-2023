bf8b1dd22f732c506f6650bb5ab515e6f53aa2a7
Daniel Araya <darayaroma@gmail.com>
2023-10-04 12:13:48 -0600

    [ADD] Sequences for resulting classes

--- clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Database.java
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Database.java
@@ -5,9 +5,13 @@ public class Database {
    public static final String SEQ_USER = "SEQ_USER";
    public static final String SEQ_DOCTOR = "SEQ_DOCTOR";
    public static final String SEQ_AGENDA = "SEQ_AGENDA";
+   public static final String SEQ_MEDICAL_EXAM = "SEQ_MEDICAL_EXAM";
+   public static final String SEQ_MEDICAL_APPOINTMENT = "SEQ_MEDICAL_APPOINTMENT";
+   public static final String SEQ_SLOTS = "SEQ_SLOTS";
    public static final String SEQ_PATIENT = "SEQ_PATIENT";
-   public static final String SEQ_PATIENT_PERSONAL_HISTORY = "SEQ_PATIENT_PERSONAL_HISTORY";
+   public static final String SEQ_PATIENT_CARE = "SEQ_PATIENT_CARE";
    public static final String SEQ_PATIENT_FAMILY_HISTORY = "SEQ_PATIENT_FAMILY_HISTORY";
+   public static final String SEQ_PATIENT_PERSONAL_HISTORY = "SEQ_PATIENT_PERSONAL_HISTORY";
    public static final String SEQ_REPORT = "SEQ_REPORT";
    public static final String SEQ_REPORT_VALUES = "SEQ_REPORT_VALUES";
    public static final String SEQ_REPORT_RECIPIENTS = "SEQ_REPORT_RECIPIENTS";
