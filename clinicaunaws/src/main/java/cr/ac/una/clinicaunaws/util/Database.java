b207fe62812b8c6b021426bb772bfe9a88c93e92
Daniel Araya <darayaroma@gmail.com>
2023-10-08 00:23:53 -0600

    [REFACTOR] ReportValues -> ReportParameters

--- clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Database.java
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Database.java
@@ -13,7 +13,7 @@ public class Database {
    public static final String SEQ_PATIENT_FAMILY_HISTORY = "SEQ_PATIENT_FAMILY_HISTORY";
    public static final String SEQ_PATIENT_PERSONAL_HISTORY = "SEQ_PATIENT_PERSONAL_HISTORY";
    public static final String SEQ_REPORT = "SEQ_REPORT";
-   public static final String SEQ_REPORT_VALUES = "SEQ_REPORT_VALUES";
+   public static final String SEQ_REPORT_PARAMETERS = "SEQ_REPORT_VALUES";
    public static final String SEQ_REPORT_RECIPIENTS = "SEQ_REPORT_RECIPIENTS";
    public static final String SEQ_GENERAL_INFORMATION = "SEQ_GENERAL_INFORMATION";
 }
