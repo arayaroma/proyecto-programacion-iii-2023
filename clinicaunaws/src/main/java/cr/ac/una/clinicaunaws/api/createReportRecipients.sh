6555d2e518fb459bccc9adc3425c5b2dd192f030
Daniel Araya <darayaroma@gmail.com>
2023-10-12 17:31:15 -0600

    [ADD] Script for creating ReportRecipients

---
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/api/createReportRecipients.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+curl -X POST -H "Content-Type: application/json" -d '{
+  "id": 0,
+  "report": {
+    "id": 2,
+    "name": "Select Users",
+    "description": "tremendisimo",
+    "query": "SELECT * FROM TBL_USER",
+    "reportDate": "2023-10-12",
+    "frequency": "MONTHLY",
+    "version": 1 
+  },
+  "email": "darayaroma@gmail.com",
+  "version": 0
+}' http://localhost:8080/clinicaunaws/api/ReportRecipientsController/create
