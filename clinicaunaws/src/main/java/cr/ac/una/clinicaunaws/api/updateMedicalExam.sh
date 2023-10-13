6ed7b646c29353e14d1bb44f3bbc46e353d6995a
Daniel Araya <darayaroma@gmail.com>
2023-10-12 20:16:30 -0600

    [ADD] Script for updating the MedicalExam

---
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/api/updateMedicalExam.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+curl -X PUT -H "Content-Type: application/json" -d '{
+  "id": 3,
+  "patientHistory":{
+    "id": 1,
+    "patient": {
+    "id": 1,
+    "name": "daniel",
+    "firstLastname": "araya",
+    "secondLastname": "roman",
+    "identification": "901140423",
+    "phoneNumber": "86420240",
+    "email": "darayaroma@gmail.com",
+    "gender": "MALE",
+    "birthDate": "2003-04-17",
+    "version": 1
+    },
+    "pathological": "string",
+    "hospitalizations": "string",
+    "surgical": "string",
+    "allergies": "string",
+    "treatments": "string",
+    "version": 1
+  }, 
+  "name": "diabetes exam",
+  "medicalExamDate": "2023-05-01",
+  "notes": "tremendous sugar in blood",
+  "version": 1
+}' http://localhost:8080/clinicaunaws/api/MedicalExamController/update
