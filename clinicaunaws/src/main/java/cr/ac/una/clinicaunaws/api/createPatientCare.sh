21242b5049f0a3ff6f4987ab8f11f1e7b2a9d89c
Daniel Araya <darayaroma@gmail.com>
2023-10-12 20:27:14 -0600

    [ADD] Script for creating the PatientCare

---
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/api/createPatientCare.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+curl -X POST -H "Content-Type: application/json" -d '{
+  "id": 0,
+  "patientCareDate": "2023-05-12",
+  "patientHistory":{
+    "id": 1,
+    "patient": {
+        "id": 1,
+        "name": "daniel",
+        "firstLastname": "araya",
+        "secondLastname": "roman",
+        "identification": "901140423",
+        "phoneNumber": "86420240",
+        "email": "darayaroma@gmail.com",
+        "gender": "MALE",
+        "birthDate": "2003-04-17",
+        "version": 1
+        },
+    "pathological": "string",
+    "hospitalizations": "string",
+    "surgical": "string",
+    "allergies": "string",
+    "treatments": "string",
+    "version": 0
+  },
+  "bloodPressure": "string",
+  "heartRate": "string",
+  "weight": "string",
+  "height": "string",
+  "temperature": "string",
+  "bodyMassIndex": "string",
+  "nursingNotes": "string",
+  "reason": "string",
+  "carePlan": "string",
+  "observations": "string",
+  "physicalExam": "string",
+  "treatment": "string",
+  "version": 0
+}' http://localhost:8080/clinicaunaws/api/PatientCareController/create
