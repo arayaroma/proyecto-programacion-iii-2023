3e93d4a628da0c11f27c1c1f2cb450bdcd296000
Daniel Araya <darayaroma@gmail.com>
2023-10-19 14:27:31 -0600

    [ADDED] API Security and Documentation ReportParameters

--- clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/controller/ReportParametersController.java
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/controller/ReportParametersController.java
@@ -3,9 +3,14 @@ package cr.ac.una.clinicaunaws.controller;
 import java.util.List;
 import java.util.logging.Logger;
 import cr.ac.una.clinicaunaws.dto.ReportParametersDto;
+import cr.ac.una.clinicaunaws.security.Secure;
 import cr.ac.una.clinicaunaws.services.ReportParametersService;
 import cr.ac.una.clinicaunaws.util.ResponseCode;
 import cr.ac.una.clinicaunaws.util.ResponseWrapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.ejb.EJB;
 import jakarta.ws.rs.DELETE;
@@ -16,21 +21,27 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.GenericEntity;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
 
 /**
  * 
  * @author arayaroma
  */
+@Secure
 @Path("/ReportParametersController")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@SecurityRequirement(name = "jwt-auth")
 @Tag(name = "ReportParametersController", description = "Manage endpoints related to the ReportParameters.")
 public class ReportParametersController {
-
     private static final Logger logger = Logger.getLogger(ReportParametersController.class.getName());
+
+    @Context
+    SecurityContext securityContext;
 
     @EJB
     ReportParametersService reportParametersService;
@@ -43,6 +54,13 @@ public class ReportParametersController {
      */
     @POST
     @Path("/create")
+    @Operation(summary = "Create a new ReportParameters", description = "Create a new ReportParameters", tags = {
+            "ReportParametersController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "ReportParameters created"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error") })
     public Response createReportParameters(ReportParametersDto reportParametersDto) {
         try {
             ResponseWrapper response = reportParametersService.createReportParameters(reportParametersDto);
@@ -64,6 +82,15 @@ public class ReportParametersController {
      */
     @GET
     @Path("/reportParameters/{id}")
+    @Operation(summary = "Get a ReportParameters by id", description = "Get a ReportParameters by id", tags = {
+            "ReportParametersController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ReportParameters found"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "ReportParameters not found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response getReportParametersById(@PathParam("id") Long id) {
         try {
             ResponseWrapper response = reportParametersService.getReportParametersById(id);
@@ -85,6 +112,15 @@ public class ReportParametersController {
     @GET
     @Path("/reportParameters")
     @SuppressWarnings("unchecked")
+    @Operation(summary = "Get all ReportParameters", description = "Get all ReportParameters", tags = {
+            "ReportParametersController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ReportParameters found"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "ReportParameters not found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response getAllReportValues() {
         try {
             ResponseWrapper response = reportParametersService.getAllReportParameters();
@@ -108,6 +144,15 @@ public class ReportParametersController {
      */
     @PUT
     @Path("/update")
+    @Operation(summary = "Update a ReportParameters", description = "Update a ReportParameters", tags = {
+            "ReportParametersController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ReportParameters updated"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "ReportParameters not found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response updateReportParameters(ReportParametersDto reportParametersDto) {
         try {
             ResponseWrapper response = reportParametersService.updateReportParameters(reportParametersDto);
@@ -129,6 +174,15 @@ public class ReportParametersController {
      */
     @DELETE
     @Path("/delete/{id}")
+    @Operation(summary = "Delete a ReportParameters by id", description = "Delete a ReportParameters by id", tags = {
+            "ReportParametersController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ReportParameters deleted"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "ReportParameters not found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response deleteReportParameters(@PathParam("id") Long id) {
         try {
             ResponseWrapper response = reportParametersService.deleteReportParameters(id);
