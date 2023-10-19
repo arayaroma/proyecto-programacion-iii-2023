d188ab957c3978fb956a7009b1dc737c3f21f444
Daniel Araya <darayaroma@gmail.com>
2023-10-19 07:53:01 -0600

    [ADD] module.info jakarta.json, transitive to javafx.grpahics

--- clinicauna/src/main/java/module-info.java
+++ clinicauna/src/main/java/module-info.java
@@ -1,25 +1,26 @@
 module Clinicauna {
-     //JAVAFX
+    // JAVAFX
     requires javafx.controls;
-    requires javafx.fxml;
+    requires transitive javafx.fxml;
     requires com.jfoenix;
     requires java.base;
     requires java.logging;
     requires java.desktop;
-   
-    //JAKARTA
+
+    // JAKARTA
     requires jakarta.xml.bind;
     requires jakarta.ws.rs;
-    //IMAGE AND FILES SERIALIZATION
+    requires jakarta.json;
+    // IMAGE AND FILES SERIALIZATION
     requires org.apache.commons.compress;
     requires org.apache.commons.io;
-    //MESSAGES
+    // MESSAGES
     requires org.controlsfx.controls;
     requires AnimateFX;
-    
-   
+
     opens cr.ac.una.clinicauna to javafx.fxml, javafx.graphics;
     opens cr.ac.una.clinicauna.controller to javafx.fxml, javafx.controls, com.jfoenix;
     opens cr.ac.una.clinicauna.components to javafx.fxml, javafx.controls, com.jfoenix;
+
     exports cr.ac.una.clinicauna.model;
 }
