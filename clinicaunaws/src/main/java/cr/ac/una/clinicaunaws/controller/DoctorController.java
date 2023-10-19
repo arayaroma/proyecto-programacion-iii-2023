49c9226325500ae5e8ba519dfceb09e20d6015c7
Daniel Araya <darayaroma@gmail.com>
2023-10-19 14:25:45 -0600

    [ADDED] API Security and Documentation Doctor

--- clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/controller/DoctorController.java
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/controller/DoctorController.java
@@ -1,9 +1,14 @@
 package cr.ac.una.clinicaunaws.controller;
 
 import cr.ac.una.clinicaunaws.dto.DoctorDto;
+import cr.ac.una.clinicaunaws.security.Secure;
 import cr.ac.una.clinicaunaws.services.DoctorService;
 import cr.ac.una.clinicaunaws.util.ResponseCode;
 import cr.ac.una.clinicaunaws.util.ResponseWrapper;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.ejb.EJB;
 import jakarta.ws.rs.Consumes;
@@ -14,23 +19,30 @@ import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.GenericEntity;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
 import java.util.List;
 import java.util.logging.Logger;
 
 /**
  *
+ * @author arayaroma
  * @author vargas
  */
+@Secure
 @Path("/DoctorController")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@SecurityRequirement(name = "jwt-auth")
 @Tag(name = "DoctorController", description = "Manage endpoints related to the Doctor.")
 public class DoctorController {
-
     private static final Logger logger = Logger.getLogger(DoctorController.class.getName());
+
+    @Context
+    SecurityContext securityContext;
 
     @EJB
     DoctorService doctorService;
@@ -43,6 +55,14 @@ public class DoctorController {
      */
     @POST
     @Path("/create")
+    @Operation(summary = "Create a new Doctor", description = "Create a new Doctor", tags = {
+            "DoctorController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Doctor created"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "409", description = "Doctor already exists"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response createDoctor(DoctorDto doctorDto) {
         try {
             System.out.println(doctorDto.toString());
@@ -65,6 +85,14 @@ public class DoctorController {
      */
     @GET
     @Path("/doctor/{id}")
+    @Operation(summary = "Get a Doctor by id", description = "Get a Doctor by id", tags = {
+            "DoctorController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Doctor found"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "404", description = "Doctor not found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response getDoctorById(@PathParam("id") Long id) {
         try {
             ResponseWrapper response = doctorService.getDoctorById(id);
@@ -86,6 +114,13 @@ public class DoctorController {
     @GET
     @Path("/doctors")
     @SuppressWarnings("unchecked")
+    @Operation(summary = "Get all Doctors", description = "Get all Doctors", tags = {
+            "DoctorController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Doctors found"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response getDoctors() {
         try {
             ResponseWrapper response = doctorService.getDoctors();
@@ -108,6 +143,14 @@ public class DoctorController {
      */
     @PUT
     @Path("/update")
+    @Operation(summary = "Update a Doctor", description = "Update a Doctor", tags = {
+            "DoctorController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Doctor updated"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response updateDoctor(DoctorDto doctorDto) {
         try {
             ResponseWrapper response = doctorService.updateDoctor(doctorDto);
@@ -129,6 +172,14 @@ public class DoctorController {
      */
     @DELETE
     @Path("/delete/{id}")
+    @Operation(summary = "Delete a Doctor by id", description = "Delete a Doctor by id", tags = {
+            "DoctorController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Doctor deleted"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response deleteDoctorById(@PathParam("id") Long id) {
         try {
             ResponseWrapper response = doctorService.deleteDoctorById(id);
