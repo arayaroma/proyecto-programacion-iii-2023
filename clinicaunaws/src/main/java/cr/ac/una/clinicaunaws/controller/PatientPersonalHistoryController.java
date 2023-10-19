89968c0bc171749f80af6f65ec520dcbc201006a
Daniel Araya <darayaroma@gmail.com>
2023-10-19 14:27:12 -0600

    [ADDED] API Security and Documentation PatientPersonalHistory

--- clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/controller/PatientPersonalHistoryController.java
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/controller/PatientPersonalHistoryController.java
@@ -3,9 +3,14 @@ package cr.ac.una.clinicaunaws.controller;
 import java.util.List;
 import java.util.logging.Logger;
 import cr.ac.una.clinicaunaws.dto.PatientPersonalHistoryDto;
+import cr.ac.una.clinicaunaws.security.Secure;
 import cr.ac.una.clinicaunaws.services.PatientPersonalHistoryService;
 import cr.ac.una.clinicaunaws.util.ResponseCode;
 import cr.ac.una.clinicaunaws.util.ResponseWrapper;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.ejb.EJB;
 import jakarta.ws.rs.Consumes;
@@ -16,21 +21,27 @@ import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.GenericEntity;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
 
 /**
  * 
  * @author arayaroma
  */
+@Secure
 @Path("/PatientPersonalHistoryController")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@SecurityRequirement(name = "jwt-auth")
 @Tag(name = "PatientPersonalHistoryController", description = "Manage endpoints related to the PatientPersonalHistory.")
 public class PatientPersonalHistoryController {
-
     private static final Logger logger = Logger.getLogger(PatientPersonalHistoryController.class.getName());
+
+    @Context
+    SecurityContext securityContext;
 
     @EJB
     PatientPersonalHistoryService patientPersonalHistoryService;
@@ -43,6 +54,14 @@ public class PatientPersonalHistoryController {
      */
     @POST
     @Path("/create")
+    @Operation(summary = "Create a new PatientPersonalHistory", description = "Create a new PatientPersonalHistory", tags = {
+            "PatientPersonalHistoryController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "PatientPersonalHistory created"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response createPatientPersonalHistory(PatientPersonalHistoryDto patientPersonalHistoryDto) {
         try {
             ResponseWrapper response = patientPersonalHistoryService
@@ -65,6 +84,13 @@ public class PatientPersonalHistoryController {
      */
     @GET
     @Path("/patientPersonalHistory/{id}")
+    @Operation(summary = "Get a PatientPersonalHistory by id", description = "Get a PatientPersonalHistory by id", tags = {
+            "PatientPersonalHistoryController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "PatientPersonalHistory found"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "404", description = "PatientPersonalHistory not found")
+    })
     public Response getPatientPersonalHistoryById(@PathParam("id") Long id) {
         try {
             ResponseWrapper response = patientPersonalHistoryService.getPatientPersonalHistoryById(id);
@@ -86,6 +112,13 @@ public class PatientPersonalHistoryController {
     @GET
     @Path("/patientPersonalHistory")
     @SuppressWarnings("unchecked")
+    @Operation(summary = "Get all PatientPersonalHistory", description = "Get all PatientPersonalHistory", tags = {
+            "PatientPersonalHistoryController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "PatientPersonalHistory found"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "404", description = "PatientPersonalHistory not found")
+    })
     public Response getAllPatientPersonalHistory() {
         try {
             ResponseWrapper response = patientPersonalHistoryService.getAllPatientPersonalHistory();
@@ -110,6 +143,14 @@ public class PatientPersonalHistoryController {
      */
     @PUT
     @Path("/update")
+    @Operation(summary = "Update a PatientPersonalHistory", description = "Update a PatientPersonalHistory", tags = {
+            "PatientPersonalHistoryController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "PatientPersonalHistory updated"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response updatePatientPersonalHistory(PatientPersonalHistoryDto patientPersonalHistoryDto) {
         try {
             ResponseWrapper response = patientPersonalHistoryService
@@ -132,6 +173,13 @@ public class PatientPersonalHistoryController {
      */
     @DELETE
     @Path("/delete/{id}")
+    @Operation(summary = "Delete a PatientPersonalHistory by id", description = "Delete a PatientPersonalHistory by id", tags = {
+            "PatientPersonalHistoryController" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "PatientPersonalHistory deleted"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden")
+    })
     public Response deletePatientPersonalHistory(@PathParam("id") Long id) {
         try {
             ResponseWrapper response = patientPersonalHistoryService.deletePatientPersonalHistory(id);
