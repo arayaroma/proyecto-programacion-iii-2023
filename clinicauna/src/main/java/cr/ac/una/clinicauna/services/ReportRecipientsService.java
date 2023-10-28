607a171841eec86452699cd460602eb38669f1cb
Daniel Araya <darayaroma@gmail.com>
2023-10-27 21:06:10 -0600

    [ADD] Services for Report, ReportParameters, ReportRecipients

---
+++ clinicauna/src/main/java/cr/ac/una/clinicauna/services/ReportRecipientsService.java
@@ -0,0 +1,153 @@
+package cr.ac.una.clinicauna.services;
+
+import java.util.HashMap;
+import java.util.List;
+import cr.ac.una.clinicauna.model.ReportRecipientsDto;
+import cr.ac.una.clinicauna.util.Request;
+import cr.ac.una.clinicauna.util.ResponseCode;
+import cr.ac.una.clinicauna.util.ResponseWrapper;
+import jakarta.ws.rs.core.GenericType;
+
+/**
+ * 
+ * @author arayaroma
+ */
+public class ReportRecipientsService {
+
+    public ResponseWrapper createReportRecipients(ReportRecipientsDto reportRecipientsDto) {
+        try {
+            Request request = new Request("ReportRecipientsController/create");
+            request.post(reportRecipientsDto);
+            if (request.isError()) {
+                return new ResponseWrapper(
+                        ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        "Error in the request: " + request.getError(),
+                        null);
+            }
+            ReportRecipientsDto reportRecipients = (ReportRecipientsDto) request.readEntity(ReportRecipientsDto.class);
+            return new ResponseWrapper(
+                    ResponseCode.CREATED.getCode(),
+                    ResponseCode.CREATED,
+                    "ReportRecipients created successfully: ",
+                    reportRecipients);
+        } catch (Exception e) {
+            return new ResponseWrapper(
+                    ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                    ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + e.toString(),
+                    null);
+        }
+    }
+
+    public ResponseWrapper getReportRecipientsById(Long id) {
+        try {
+            HashMap<String, Object> params = new HashMap<>();
+            params.put("id", id);
+            Request request = new Request("ReportRecipientsController/reportRecipients", "/{id}", params);
+            request.get();
+            if (request.isError()) {
+                return new ResponseWrapper(
+                        ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        "Error in the request: " + request.getError(),
+                        null);
+            }
+            ReportRecipientsDto reportRecipients = (ReportRecipientsDto) request.readEntity(ReportRecipientsDto.class);
+            return new ResponseWrapper(
+                    ResponseCode.OK.getCode(),
+                    ResponseCode.OK,
+                    "ReportRecipients retrieved successfully: ",
+                    reportRecipients);
+        } catch (Exception e) {
+            return new ResponseWrapper(
+                    ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                    ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + e.toString(),
+                    null);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public ResponseWrapper getAllReportRecipients() {
+        try {
+            Request request = new Request("ReportRecipientsController/reportRecipients");
+            request.get();
+            if (request.isError()) {
+                return new ResponseWrapper(
+                        ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        "Error in the request: " + request.getError(),
+                        null);
+            }
+            List<ReportRecipientsDto> reportRecipients = (List<ReportRecipientsDto>) request
+                    .readEntity(new GenericType<List<ReportRecipientsDto>>() {
+                    });
+            return new ResponseWrapper(
+                    ResponseCode.OK.getCode(),
+                    ResponseCode.OK,
+                    "ReportRecipients retrieved successfully: ",
+                    reportRecipients);
+        } catch (Exception e) {
+            return new ResponseWrapper(
+                    ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                    ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + e.toString(),
+                    null);
+        }
+    }
+
+    public ResponseWrapper updateReportRecipients(ReportRecipientsDto reportRecipientsDto) {
+        try {
+            Request request = new Request("ReportRecipientsController/update");
+            request.put(reportRecipientsDto);
+            if (request.isError()) {
+                return new ResponseWrapper(
+                        ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        "Error in the request: " + request.getError(),
+                        null);
+            }
+            ReportRecipientsDto reportRecipients = (ReportRecipientsDto) request.readEntity(ReportRecipientsDto.class);
+            return new ResponseWrapper(
+                    ResponseCode.OK.getCode(),
+                    ResponseCode.OK,
+                    "ReportRecipients updated successfully: ",
+                    reportRecipients);
+        } catch (Exception e) {
+            return new ResponseWrapper(
+                    ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                    ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + e.toString(),
+                    null);
+        }
+    }
+
+    public ResponseWrapper deleteReportRecipients(Long id) {
+        try {
+            HashMap<String, Object> params = new HashMap<>();
+            params.put("id", id);
+            Request request = new Request("ReportRecipientsController/delete", "/{id}", params);
+            request.delete();
+            if (request.isError()) {
+                return new ResponseWrapper(
+                        ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        "Error in the request: " + request.getError(),
+                        null);
+            }
+            return new ResponseWrapper(
+                    ResponseCode.NO_CONTENT.getCode(),
+                    ResponseCode.NO_CONTENT,
+                    "ReportRecipients deleted successfully: ",
+                    null);
+        } catch (Exception e) {
+            return new ResponseWrapper(
+                    ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                    ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + e.toString(),
+                    null);
+        }
+    }
+
+}
--- clinicauna/src/main/java/cr/ac/una/clinicauna/services/ReportService.java
+++ clinicauna/src/main/java/cr/ac/una/clinicauna/services/ReportService.java
@@ -3,6 +3,7 @@ package cr.ac.una.clinicauna.services;
 import cr.ac.una.clinicauna.util.Request;
 import cr.ac.una.clinicauna.util.ResponseCode;
 import cr.ac.una.clinicauna.util.ResponseWrapper;
+import jakarta.ws.rs.core.GenericType;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -11,13 +12,151 @@ import java.io.OutputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.awt.Desktop;
+import java.util.List;
+import cr.ac.una.clinicauna.model.ReportDto;
 import cr.ac.una.clinicauna.util.FileLoader;
 
 /**
  *
  * @author vargas
+ * @author arayaroma
  */
 public class ReportService {
+
+    public ResponseWrapper createReport(ReportDto reportDto) {
+        try {
+            Request request = new Request("ReportController/create");
+            request.post(reportDto);
+            if (request.isError()) {
+                return new ResponseWrapper(
+                        ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        "Error in the request: " + request.getError(),
+                        null);
+            }
+            ReportDto report = (ReportDto) request.readEntity(ReportDto.class);
+            return new ResponseWrapper(
+                    ResponseCode.CREATED.getCode(),
+                    ResponseCode.CREATED,
+                    "Report created successfully: ",
+                    report);
+        } catch (Exception ex) {
+            return new ResponseWrapper(
+                    ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                    ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + ex.toString(),
+                    null);
+        }
+    }
+
+    public ResponseWrapper getReport(Long id) {
+        try {
+            HashMap<String, Object> params = new HashMap<>();
+            params.put("id", id);
+            Request request = new Request("ReportController/report", "/{id}", params);
+            request.get();
+            if (request.isError()) {
+                return new ResponseWrapper(
+                        ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        "Error in the request: " + request.getError(),
+                        null);
+            }
+            ReportDto report = (ReportDto) request.readEntity(ReportDto.class);
+            return new ResponseWrapper(
+                    ResponseCode.OK.getCode(),
+                    ResponseCode.OK,
+                    "Report retrieved successfully: ",
+                    report);
+        } catch (Exception ex) {
+            return new ResponseWrapper(
+                    ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                    ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + ex.toString(),
+                    null);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public ResponseWrapper getAllReports() {
+        try {
+            Request request = new Request("ReportController/report");
+            request.get();
+            if (request.isError()) {
+                return new ResponseWrapper(
+                        ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        "Error in the request: " + request.getError(),
+                        null);
+            }
+            List<ReportDto> reportDtos = (List<ReportDto>) request.readEntity(new GenericType<List<ReportDto>>() {
+            });
+            return new ResponseWrapper(
+                    ResponseCode.OK.getCode(),
+                    ResponseCode.OK,
+                    "Reports retrieved successfully: ",
+                    reportDtos);
+        } catch (Exception ex) {
+            return new ResponseWrapper(
+                    ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                    ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + ex.toString(),
+                    null);
+        }
+    }
+
+    public ResponseWrapper updateReport(ReportDto reportDto) {
+        try {
+            Request request = new Request("ReportController/update");
+            request.put(reportDto);
+            if (request.isError()) {
+                return new ResponseWrapper(
+                        ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        "Error in the request: " + request.getError(),
+                        null);
+            }
+            ReportDto report = (ReportDto) request.readEntity(ReportDto.class);
+            return new ResponseWrapper(
+                    ResponseCode.OK.getCode(),
+                    ResponseCode.OK,
+                    "Report updated successfully: ",
+                    report);
+        } catch (Exception ex) {
+            return new ResponseWrapper(
+                    ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                    ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + ex.toString(),
+                    null);
+        }
+    }
+
+    public ResponseWrapper deleteReport(Long id) {
+        try {
+            HashMap<String, Object> params = new HashMap<>();
+            params.put("id", id);
+            Request request = new Request("ReportController/delete", "/{id}", params);
+            request.delete();
+            if (request.isError()) {
+                return new ResponseWrapper(
+                        ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        "Error in the request: " + request.getError(),
+                        null);
+            }
+            return new ResponseWrapper(
+                    ResponseCode.NO_CONTENT.getCode(),
+                    ResponseCode.NO_CONTENT,
+                    "Report deleted successfully: ",
+                    null);
+        } catch (Exception ex) {
+            return new ResponseWrapper(
+                    ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
+                    ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + ex.toString(),
+                    null);
+        }
+    }
 
     public ResponseWrapper createPatientReport(Long id) {
         try {
@@ -26,7 +165,7 @@ public class ReportService {
                 return new ResponseWrapper(ResponseCode.CONFLICT.getCode(), ResponseCode.CONFLICT, "Path is required",
                         null);
             }
-            HashMap params = new HashMap();
+            HashMap<String, Object> params = new HashMap<>();
             params.put("id", id);
             Request request = new Request("ReportController/createPatientReport", "/{id}", params);
             request.get();
@@ -44,28 +183,30 @@ public class ReportService {
                     ResponseCode.INTERNAL_SERVER_ERROR, "Error creating the PDF",
                     null);
         } catch (Exception ex) {
-            return new ResponseWrapper(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), ResponseCode.INTERNAL_SERVER_ERROR, "Error in the service: " + ex.toString(), null);
+            return new ResponseWrapper(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + ex.toString(), null);
         }
     }
 
     public ResponseWrapper createAgendaReport(Long DoctorId, String sDate, String eDate) {
         try {
-            HashMap params = new HashMap();
+            HashMap<String, Object> params = new HashMap<>();
             params.put("doctorId", DoctorId);
             params.put("startDate", sDate);
             params.put("endDate", eDate);
-            Request request = new Request("ReportController/createAgendaReport", "/{doctorId}/{startDate}/{endDate}", params);
+            Request request = new Request("ReportController/createAgendaReport", "/{doctorId}/{startDate}/{endDate}",
+                    params);
             request.get();
 
             if (request.isError()) {
                 return new ResponseWrapper(ResponseCode.INTERNAL_SERVER_ERROR.getCode(),
                         ResponseCode.INTERNAL_SERVER_ERROR, "Error in the request: "
-                        + request.getError(),
+                                + request.getError(),
                         null);
             }
 
             byte[] pdf = (byte[]) request.readEntity(byte[].class);
-            //ESTA CREANDO EL PDF EN EL DIRECTORIO RAIZ
+            // ESTA CREANDO EL PDF EN EL DIRECTORIO RAIZ
             try (InputStream b = new ByteArrayInputStream(pdf); OutputStream out = new FileOutputStream("new.pdf")) {
 
                 int tamInput;
@@ -86,7 +227,8 @@ public class ReportService {
                     pdf);
 
         } catch (Exception ex) {
-            return new ResponseWrapper(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), ResponseCode.INTERNAL_SERVER_ERROR, "Error in the service: " + ex.toString(), null);
+            return new ResponseWrapper(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), ResponseCode.INTERNAL_SERVER_ERROR,
+                    "Error in the service: " + ex.toString(), null);
         }
     }
 
