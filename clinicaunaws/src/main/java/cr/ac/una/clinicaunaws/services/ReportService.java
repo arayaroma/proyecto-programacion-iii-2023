8eff36eaa396819217eeafaa3a70bbbe6833289b
Daniel Araya <darayaroma@gmail.com>
2023-10-29 19:11:02 -0600

    [ADD] getQueryResult method, need to Genericalize it

--- clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/services/ReportService.java
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/services/ReportService.java
@@ -18,8 +18,11 @@ import java.util.List;
 import java.util.Map;
 import static cr.ac.una.clinicaunaws.util.PersistenceContext.PERSISTENCE_UNIT_NAME;
 import cr.ac.una.clinicaunaws.dto.ReportDto;
+import cr.ac.una.clinicaunaws.dto.UserDto;
 import cr.ac.una.clinicaunaws.entities.Report;
+import cr.ac.una.clinicaunaws.entities.User;
 import cr.ac.una.clinicaunaws.util.Constants;
+import cr.ac.una.clinicaunaws.util.DtoMapper;
 import java.net.URL;
 
 /**
@@ -35,19 +38,53 @@ public class ReportService {
     private EntityManager em;
 
     /**
+     * FIXME: Make it generic for any entity & dto
+     * Get the result of a query and set it to the report
+     * 
+     * @param <T>         Entity class type
+     * @param <D>         Dto class type
+     * @param report      Report to be set
+     * @param entityClass Entity class
+     * @param dtoClass    Dto class
+     * @return List of Dto
+     */
+    @SuppressWarnings("unchecked")
+    public <T, D> List<D> getQueryResult(ReportDto report, Class<T> entityClass, Class<D> dtoClass) {
+        if (report == null) {
+            return null;
+        }
+        Query query = em.createNativeQuery(report.getQuery(), entityClass);
+        List<T> entityList = (List<T>) query.getResultList();
+        List<D> dtoList = new ArrayList<>();
+
+        for (T entity : entityList) {
+            D dto = DtoMapper.convertToDto(entity, dtoClass);
+            dtoList.add(dto);
+        }
+        report.getQueryManager().setResult(dtoList);
+        report.getQueryManager().setStatus(ResponseCode.OK.getCode().toString());
+        return dtoList;
+    }
+
+    /**
      * Create a new Report
      *
      * @param reportDto to be created
      * @return ResponseWrapper with the created Report
      */
+    @SuppressWarnings("unchecked")
     public ResponseWrapper createReport(ReportDto reportDto) {
         try {
             Report report = reportDto.convertFromDTOToEntity(reportDto, new Report(reportDto));
             em.persist(report);
             em.flush();
+
+            List<UserDto> result = getQueryResult(reportDto, User.class, UserDto.class);
+            reportDto.getQueryManager().setResult(result);
+
             return new ResponseWrapper(
-                    ResponseCode.OK.getCode(),
-                    ResponseCode.OK,
+                    ResponseCode.CREATED.getCode(),
+                    ResponseCode.CREATED,
                     "Report created.",
                     reportDto.convertFromEntityToDTO(report, reportDto));
         } catch (Exception e) {
@@ -191,7 +228,7 @@ public class ReportService {
      *
      * @param id patient id to be retrieved
      * @return ResponseWrapper with the response and report from database, or
-     * null if an exception occurred
+     *         null if an exception occurred
      * @throws java.io.IOException
      * @throws net.sf.jasperreports.engine.JRException
      */
@@ -209,44 +246,43 @@ public class ReportService {
                 absolutePath = resource.getFile();
                 System.out.println("Ruta absoluta del archivo: " + absolutePath);
             }
-                JasperReport jReport = JasperCompileManager.compileReport(absolutePath);
-                if (jReport == null) {
-                    System.out.println("ERROR: NO EXISTE EL JASPER");
-                }
+            JasperReport jReport = JasperCompileManager.compileReport(absolutePath);
+            if (jReport == null) {
+                System.out.println("ERROR: NO EXISTE EL JASPER");
+            }
 
-                Map<String, Object> par = new HashMap<>();
-                par.put("idPatientCare", id);
+            Map<String, Object> par = new HashMap<>();
+            par.put("idPatientCare", id);
 
-                try (Connection connection = DriverManager.getConnection(Constants.URL_DB, Constants.USER_DB,
-                        Constants.PASS_DB)) {
-                    // Llenar el informe con datos y la conexión a la base de datos
-                    JasperPrint jasperPrint = JasperFillManager.fillReport(jReport, par, connection);
-                    byte[] pdfbytes = JasperExportManager.exportReportToPdf(jasperPrint);
-                    return new ResponseWrapper(
-                            ResponseCode.OK.getCode(),
-                            ResponseCode.OK,
-                            "Report created successfully.",
-                            pdfbytes);
-                } catch (SQLException e) {
-                    return new ResponseWrapper(
-                            ResponseCode.CONFLICT.getCode(),
-                            ResponseCode.CONFLICT,
-                            "No connected: " + e.getMessage(),
-                            null);
-                }
-            }catch (JRException e) {
+            try (Connection connection = DriverManager.getConnection(Constants.URL_DB, Constants.USER_DB,
+                    Constants.PASS_DB)) {
+                // Llenar el informe con datos y la conexión a la base de datos
+                JasperPrint jasperPrint = JasperFillManager.fillReport(jReport, par, connection);
+                byte[] pdfbytes = JasperExportManager.exportReportToPdf(jasperPrint);
+                return new ResponseWrapper(
+                        ResponseCode.OK.getCode(),
+                        ResponseCode.OK,
+                        "Report created successfully.",
+                        pdfbytes);
+            } catch (SQLException e) {
+                return new ResponseWrapper(
+                        ResponseCode.CONFLICT.getCode(),
+                        ResponseCode.CONFLICT,
+                        "No connected: " + e.getMessage(),
+                        null);
+            }
+        } catch (JRException e) {
             // Manejar la excepción (log, enviar alerta, etc.)
             e.printStackTrace();
             throw new RuntimeException("Error al generar el informe", e);
         }
-        }
-    
-    
+    }
 
-    public ResponseWrapper createAgendaReport(Long doctorId, String startDate, String endDate) throws IOException, JRException {
+    public ResponseWrapper createAgendaReport(Long doctorId, String startDate, String endDate)
+            throws IOException, JRException {
         // FALTA HACER REFRACTOR A ESTE CODIGO
         try {
-            
+
             String absolutePath = "";
             String jasperPath = "jrxml/agendaReport.jrxml";
             ClassLoader classLoader = getClass().getClassLoader();
@@ -258,7 +294,7 @@ public class ReportService {
                 absolutePath = resource.getFile();
                 System.out.println("Ruta absoluta del archivo: " + absolutePath);
             }
-                JasperReport jReport = JasperCompileManager.compileReport(absolutePath);
+            JasperReport jReport = JasperCompileManager.compileReport(absolutePath);
 
             if (jReport == null) {
                 System.out.println("ERROR: NO EXISTE EL JASPER");
