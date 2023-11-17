73412729d78ccbe0b8355cad3f6e0d65753d6af9
enajera123 <estebannajera42@gmail.com>
2023-11-17 15:36:22 -0600

    [FIX] Report generated when it correspond

--- clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Scheduler.java
+++ clinicaunaws/src/main/java/cr/ac/una/clinicaunaws/util/Scheduler.java
@@ -23,25 +23,25 @@ import java.util.stream.Collectors;
  */
 @Stateless
 public class Scheduler {
-    
+
     @EJB
     MedicalAppointmentService MaService;
-    
+
     @EJB
     EmailService eService;
-    
+
     @EJB
     ReportService rService;
     @EJB
     ReportParametersService reportParametersService;
-    
+
     private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    
+
     @Schedule(second = "0", minute = "*", hour = "*")
     public void showTime() {
         System.out.print("entra");
     }
-    
+
     @Schedule(second = "0", minute = "*", hour = "*")
     public void checkReminders() {
         checkReports();
@@ -51,7 +51,7 @@ public class Scheduler {
     public void checkMedicalReminders() {
         checkAppointments();
     }
-    
+
     public void checkAppointments() {
         try {
             List<MedicalAppointmentDto> appointments = (List<MedicalAppointmentDto>) MaService.getAllMedicalAppointments().getData();
@@ -72,7 +72,7 @@ public class Scheduler {
             System.out.println(e.toString());
         }
     }
-    
+
     public void checkReports() {
         try {
             List<ReportDto> reports = (List<ReportDto>) rService.getAllReports().getData();
@@ -91,24 +91,24 @@ public class Scheduler {
                 }
             }
         } catch (Exception e) {
-            
+
         }
     }
-    
+
     private Predicate<MedicalAppointmentDto> isSameDayApp(LocalDate compareDate) {
         return appointment -> {
             LocalDate appointmentDate = LocalDate.parse(appointment.getScheduledDate(), dateFormatter);
             return appointmentDate.isEqual(compareDate);
         };
     }
-    
+
     private Predicate<ReportDto> isSameDayRep(LocalDate compareDate) {
         return report -> {
             LocalDate reportDate = LocalDate.parse(report.getReportDate(), dateFormatter);
             return reportDate.isEqual(compareDate);
         };
     }
-    
+
     private void changeReportDate(ReportDto report) {
         switch (report.getFrequency()) {
             case "ANNUALLY" ->
@@ -119,10 +119,12 @@ public class Scheduler {
                 report.setReportDate(plusWeeks(report.getReportDate(), 1));
             case "DAILY" ->
                 report.setReportDate(plusDays(report.getReportDate(), 1));
+            case "ONCE" ->
+                report.setReportDate(plusDays(report.getReportDate(), -1));
             default -> {
             }
         }
-        
+
         List<ReportParametersDto> parameters = report.getReportParameters();
         for (ReportParametersDto parameter : parameters) {
             if (isDateString(parameter.getValue())) {
@@ -141,27 +143,27 @@ public class Scheduler {
             }
         }
     }
-    
+
     private String plusYears(String date, int years) {
         LocalDate localDate = LocalDate.parse(date, dateFormatter).plusYears(years);
         return localDate.format(dateFormatter);
     }
-    
+
     private String plusMonths(String date, int months) {
         LocalDate localDate = LocalDate.parse(date, dateFormatter).plusMonths(months);
         return localDate.format(dateFormatter);
     }
-    
+
     private String plusWeeks(String date, int weeks) {
         LocalDate localDate = LocalDate.parse(date, dateFormatter).plusWeeks(weeks);
         return localDate.format(dateFormatter);
     }
-    
+
     private String plusDays(String date, int days) {
         LocalDate localDate = LocalDate.parse(date, dateFormatter).plusDays(days);
         return localDate.format(dateFormatter);
     }
-    
+
     private boolean isDateString(String value) {
         try {
             LocalDate.parse(value, dateFormatter);
